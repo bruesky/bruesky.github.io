bb6f68958130fc63d9e0b6828cadba55eea7ac6b
Xiangkui Guo <bruce1988cn@gmail.com>
2019-09-02 13:59:05 -0500

    add wap folder

---
+++ learnjs/hello.js
@@ -0,0 +1,12 @@
+'use strict';
+const lib = require("./lib.js");
+
+function greeting() {
+    console.log(lib.add(2,3));
+}
+
+function main() {
+    greeting();
+}
+
+main();
---
+++ learnjs/lib.js
@@ -0,0 +1,7 @@
+
+function add(x,y) {
+    return x+y;
+}
+
+module.exports.add = add;
+console.log(module.exports)
